6c365b4d4360096e65725fb5267c9e1b85874c53
YuryKoropenko <yurykoropenko@gmail.com>
2018-12-27 11:56:41 +0200

    27.12.2018

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
 $(function() {
+	/* Выпадающее меню */
+	$('.h-nav__item-submenu').hover(function() {
+		$(this).children('.h-nav__submenu-wp').stop(false, true).fadeIn(200);
+	}, function() {
+        $(this).children('.h-nav__submenu-wp').stop(false, true).fadeOut(200);
+	});
+
 	/* Маска телефона */
 	$('.phone').mask('+7(999)999-99-99');
 
