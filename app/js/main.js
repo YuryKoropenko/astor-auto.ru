fd0f629482ce5f0ba57794bab66ae96b02e1d3c8
YuryKoropenko <yurykoropenko@gmail.com>
2019-01-08 18:01:33 +0200

    08.01.2019

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,45 @@ $(function() {
 	/* CallBack форма */
 	$('.h-callback__btn').fancybox();
 	$('.p-contacts__callback').fancybox();
+
+	/* mob-footer */
+    if($(window).width() < 768) {
+        $('.f-menu .footer__title').on('click', function () {
+            $(this).toggleClass('active');
+            $(this).parent().children('.f-menu__list').slideToggle(200);
+            return false;
+        });
+        $('.f-addr .footer__title').on('click', function () {
+            $(this).toggleClass('active');
+            $(this).parent().children('.f-addr__hidden').slideToggle(200);
+            return false;
+        });
+    }
+
+    /* mob-menu */
+    $('.h-mobnaw').on('click', function() {
+    	$('.header__mobmenu').show();
+    	return false;
+	});
+    $('.h-mobmenu__close').on('click', function() {
+        $('.header__mobmenu').hide();
+        return false;
+	});
+    $('.h-mobmenu__item-submenu .h-mobmenu__link').on('click', function() {
+		 $(this).parent().children('.h-mobmenu__sublist').slideToggle(200);
+		 $(document).click(function(event) {
+		 if ($(event.target).closest('.h-mobmenu__sublist').length) return;
+		 $('.h-mobmenu__sublist').slideUp(200);
+		 event.stopPropagation();
+		 });
+        return false;
+    });
+
+
+    $(document).click(function(event) {
+        if ($(event.target).closest('.header__mobmenu').length) return;
+        $('.header__mobmenu').hide();
+        event.stopPropagation();
+    });
+    return false;
 });
